209575042d48cce9e739ae9299d113674ef00a3d
arjunmdas <arjundas.27586@gmail.com>
2023-05-24 07:54:18 +0530

    [Code navigation] Fixed code navigation

---
+++ ide/code-navigation.el
@@ -0,0 +1,29 @@
+(setq path-to-ctags "/opt/homebrew/bin/ctags") ;; should be the location of your ctags installation
+(defun create-tags (dir-name)
+    "Create tags file."
+    (interactive "DDirectory: ")
+    (shell-command
+     (format "%s -f TAGS -e -R %s" path-to-ctags (directory-file-name dir-name))))
+
+;; To close tags buffer after navigation
+(defun close-xref-buffer-after-navigation ()
+  "Close the *xref* buffer after navigation."
+  (when-let (buf (get-buffer "*xref*"))
+    (kill-buffer buf))
+  (delete-duplicate-windows))
+
+(defun delete-duplicate-windows ()
+  "Remove all windows displaying the current buffer, except the current window."
+  (let ((current-buffer (current-buffer)))
+    (walk-windows
+     (lambda (window)
+       (when (and (not (eq window (selected-window)))
+                  (eq (window-buffer window) current-buffer))
+         (delete-window window)))
+     nil
+     'visible)))
+
+(add-hook 'next-error-hook 'close-xref-buffer-after-navigation)
+
+
+
--- init.el
+++ init.el
@@ -41,23 +41,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/icloud/personal/todos.org"))
+ '(org-agenda-files
+   '("~/icloud/personal/recurring.org" "/Users/arjundas/icloud/personal/2023/q2.org"))
  '(org-capture-templates
-   '(("t" "General todo" entry
-      (file "~/icloud/personal/todos.org")
-      "* TODO %^ %?
-DEADLINE: %^t")
-     ("b" "Blog Idea" entry
-      (file "~/icloud/personal/todos.org")
-      "* TODO [Blog] %^ %?
-DEADLINE: %^t")
-     ("r" "Rubocop" entry
-      (file "~/icloud/personal/todos.org")
-      "* TODO [Rubocop] ")
-     ("g" "Game" entry
-      (file "~/icloud/personal/todos.org")
-      "* TODO [Game] %^ %?
-DEADLINE: %^t"))))
+   '(("r" "Recurring todo" entry
+      (file "~/icloud/personal/recurring.org")
+      "* RECU [#%^{In how many days to be completed?|3|6|13|17|23|}] %^{Item description} 
+DEADLINE: %^t
+:PROPERTIES:
+:TRIGGER: self deadline!(\"+%^{In how many days to be completed?|3|6|13|17|23|}weekday\") todo!(\"RECU\")
+:END:" :immediate-finish t)
+     ("t" "todo" entry
+      (file "~/icloud/personal/2023/q2.org")
+      "* TODO [#%^{In how many days to be completed?|3|6|13|17|}] %^{Item description} 
+DEADLINE: %^t" :immediate-finish t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
